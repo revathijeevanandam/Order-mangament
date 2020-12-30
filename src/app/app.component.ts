9e2d4a9d091c34b5dbb4a1be305e868381969103
Revathi <jrevathi28@gmail.com>
2020-12-30 17:31:27 +0530

    Order management completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,45 +1,132 @@
 import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+interface Food {
+  value: string;
+
+  price:number;
+
+}
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent implements OnInit {
-  // title = 'simplepjt';
 
-  addUserForm: FormGroup;
-
-  isFormSubmitted = false;
-
-  constructor(private formBuilder: FormBuilder) {}
-
-  ngOnInit(){
-
-    // Patterns
-    const PAT_NAME = "^[a-zA-Z ]{2,20}$";
-    const PAT_EMAIL = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+[.][a-zA-Z]{2,4}$";
-
-    this.addUserForm = this.formBuilder.group({
-      name: ['', [Validators.required, Validators.pattern(PAT_NAME)]],
-      email: ['', [Validators.required, Validators.pattern(PAT_EMAIL)]],
-      address: ['', Validators.required],
-      password: ['', [Validators.required, Validators.minLength(8)]]
-    });
+  selectedfood: string;
+  selecteduser: string;
+  selectedsearch:string;
+  searchText:string;
+  Quantity:number;
+  today:any;
+  order:any[]=[];
+  price;
+  total;
+  table:any[] = [];
+  constructor() {}
+  // edit(product){
+  //   var orderrv = JSON.parse(localStorage.getItem('order'));
+  //   const removeitem = orderrv.findIndex(s => s.product === product);
+  //   //if Productid matched remove taht particulat item from local-strg table
     
+  //     localStorage.setItem('order', JSON.stringify(orderrv));
+     
+    
+  //   console.log(orderrv);
+  //   this.table = JSON.parse(localStorage.getItem('order'));
+  // }
+  delete(product){
+    var orderrv = JSON.parse(localStorage.getItem('order'));
+			const removeitem = orderrv.findIndex(s => s.product === product);
+			//if Productid matched remove taht particulat item from local-strg table
+				orderrv.splice(removeitem, 1);
+        localStorage.setItem('order', JSON.stringify(orderrv));
+      console.log(orderrv);
+      this.table = JSON.parse(localStorage.getItem('order'));
+      this.selected = this.table;
   }
-   // Submit User Form
-  submitUser() {
-
-    // Set flag to true
-    this.isFormSubmitted = true;
-
-    // Return if form is invalid
-    if (this.addUserForm.invalid) {
-      return;
-    }
-    console.log('Submit', this.addUserForm.value);
+add(){
+  this.order = this.table;
+  this.price = ( this.foods.filter(foods => foods.value === this.selectedfood));
+  this.today = new Date().toLocaleString();
+  this.price=this.price[0].price;
+  this.total=this.price*this.Quantity;
+  if(this.Quantity >= 3 && (this.selectedfood == 'pepsi' || this.selectedfood == 'cola')){
+    this.total = this.total - (this.total * (20/100)) ;
   }
+  this.order.push({'user':this.selecteduser,
+    'product':this.selectedfood,
+    'price':this.price,
+    'quantity':this.Quantity ,
+    'total':this.total,'date':this.today});
+    localStorage.setItem('order',JSON.stringify( this.order));
+    console.log(this.order);
+}
+searchfn(searchText)
+{
+  this.selected =  this.table.filter(table => table.user === searchText || table.product === searchText);
+  if(searchText == null || searchText == undefined || searchText == ''){
+    this.selected = this.table;
+  }
+  // console.log(this.table);
+
+}
+selected:any;
+fn(value){
+  if(value == "All time"){
+    this.selected = this.table;
+    console.log(this.selected);
+  }
+  else if(value == "Last 7 days"){
+    var date = new Date().toLocaleString();
+    var pastDate = new Date(Date.now() - 7 * 24 * 60 * 60 * 1000).toLocaleString();
+    this.selected = this.table.filter(table => table.date > pastDate);
+    console.log(this.selected);
+  }
+  else if(value == "Today"){
+    var d = new Date();
+    d.setHours(0,0,0,0);
+    var date = new Date().toLocaleDateString();
+    console.log(date);
+    this.selected = this.table.filter(table => table.date > date);
+    console.log(this.selected);
+  }
+}
+  ngOnInit(){
+    this.selectedsearch = 'All time';
+    this.table = JSON.parse( localStorage.getItem('order')) || [];
+    this.selected = this.table;
+    //Get today's date using the JavaScript Date object.
+    var ourDate = new Date();
+    //Change it so that it is 7 days in the past.
+    var pastDate = new Date(Date.now() - 7 * 24 * 60 * 60 * 1000).toLocaleString();
+    //Log the date to our web console.
+    console.log(ourDate);
+}
+ 
+  foods: Food[] = [
+    {value: 'pepsi',price:200},
+    {value: 'cola',price:120},
+    {value: 'fanta',price:150}
+  ];
+  
+  username = [
+    { value : 'john' },
+    {  value : 'mike' },
+    {  value : 'peter' },
+    {  value : 'nick' },
+    { value : 'charls' },
+
+  ];
+  search = [
+    { value : 'All time' },
+    { value : 'Last 7 days' },
+    { value : 'Today' },
+
+
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSelectModule} from '@angular/material/select';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -12,7 +15,9 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatSelectModule,MatToolbarModule,MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
